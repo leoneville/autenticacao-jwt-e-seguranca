58a1c25b74e471ad91ab0bf57afcea5849f60641
leoneville <leoneville_@hotmail.com>
2024-11-16 19:20:50 -0300

    configs: Adding .env to project

---
+++ src/configs/__init__.py
@@ -0,0 +1,4 @@
+# pylint: disable=import-error
+from dotenv import load_dotenv
+
+load_dotenv()
--- src/drivers/jwt_handler.py
+++ src/drivers/jwt_handler.py
@@ -1,19 +1,24 @@
 from datetime import datetime, timedelta, timezone
 import jwt
+from src.configs.jwt_configs import jwt_infos
 
 
 class JwtHandler:
     def create_jwt_token(self, body: dict = None) -> str:
         token = jwt.encode(
             payload={
-                "exp": datetime.now(timezone.utc) + timedelta(minutes=1),
+                "exp": datetime.now(timezone.utc)
+                + timedelta(minutes=jwt_infos["JWT_HOURS"]),
                 "email": "meuemail",
                 **body,
             },
-            key="minhaChave",
+            key=jwt_infos["KEY"],
+            algorithm=jwt_infos["ALGORITHM"],
         )
         return token
 
     def decode_jwt_token(self, token: str) -> dict:
-        token_information = jwt.decode(token, key="minhaChave", algorithms="HS256")
+        token_information = jwt.decode(
+            token, key=jwt_infos["KEY"], algorithms=jwt_infos["ALGORITHM"]
+        )
         return token_information
